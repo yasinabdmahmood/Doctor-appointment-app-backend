178f8683df37a1f358458acf42e3d4c0606b0c27
benardop <benard.oc.p@gmail.com>
2023-01-16 19:55:31 +0300

    finished creating model tables

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :doctor
   belongs_to :user
 
-  validates :date, presence :true
-  validates :city, presence :true
+  validates :date, presence: true
+  validates :city, presence: true
 end
