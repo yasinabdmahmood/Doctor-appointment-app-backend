5795d1b442b35ef814b9c12034c8b3aa7d56c956
yasinabdmahmood <abd181103@gmail.com>
2023-01-16 12:28:58 +0300

    Implement create action of Doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,7 +4,30 @@ class DoctorsController < ApplicationController
     render json: @doctors.to_json
   end
 
-  def create; end
+  def create
+    doctor = Doctor.new(user_params)
+    if admin_user?
+      if doctor.save
+        render json: { status: "success", message: "Doctor created successfully" }, status: :created
+      else 
+        render json: { status: "error", errors: doctor.errors }, status: :unprocessable_entity
+      end
+    else
+      render json: { status: "error", message: "You are not admin" }, status: :unauthorized
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def user_params
+    params.permit(:name, :picture, :speciality, :bio)
+  end
+
+  def admin_user?
+    current_user_id = @current_user.id
+    admin = Admin.find_by(user_id: current_user_id)
+    admin.present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'doctors/index'
-  get 'doctors/create'
+  post 'doctors/create'
   get 'doctors/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
