bb30a015d689ca18c1514ca2ada2abbdc49edaad
yasinabdmahmood <abd181103@gmail.com>
2023-01-16 21:14:11 +0300

    Add Admin model and implement association with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   require 'securerandom'
   has_secure_password
   has_many :reservations, dependent: :destroy
+  has_one :admin
 
   validates :email, presence: true
   validates :password, presence: true
