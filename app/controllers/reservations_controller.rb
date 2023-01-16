09eb066ac04d6e8723ef7b2115862c0bd0704b3d
yasinabdmahmood <abd181103@gmail.com>
2023-01-16 13:27:32 +0300

    Implement create action of ReservationsController

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,5 +5,19 @@ class ReservationsController < ApplicationController
   end
 
   def create
+    def create
+      @reservation = @current_user.reservations.build(reservation_params)
+      if @reservation.save
+        render json: @reservation, status: :created
+      else
+        render json: @reservation.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def reservation_params
+      params.require(:reservation).permit(:doctor_id, :date, :city)
+    end
   end
 end
