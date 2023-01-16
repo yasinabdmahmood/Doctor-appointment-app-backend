396906faf3d3b9b48b311eee8c73fe0a7b6e6f6f
yasinabdmahmood <abd181103@gmail.com>
2023-01-16 12:49:25 +0300

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'doctors/index'
-  post 'doctors/create'
-  get 'doctors/destroy'
+  resources :doctors, only: [:index, :destroy, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
   post '/auth/login', to: 'authentication#login'
