95540c7c634c6102ad47694686d89ea250edc85a
yasinabdmahmood <abd181103@gmail.com>
2023-01-16 11:48:16 +0300

    Fix rubocop errors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,10 +1,7 @@
 class DoctorsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def create
-  end
+  def create; end
 
-  def destroy
-  end
+  def destroy; end
 end
--- test/controllers/doctors_controller_test.rb
+++ test/controllers/doctors_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class DoctorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get doctors_index_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get doctors_create_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     get doctors_destroy_url
     assert_response :success
   end
