2d3f46068d51cb5d970b1e4f01f31abd9da35762
yasinabdmahmood <abd181103@gmail.com>
2023-01-24 19:57:36 +0300

    Fix rubocop error

--- app/controllers/render_controller.rb
+++ app/controllers/render_controller.rb
@@ -1,6 +1,6 @@
 class RenderController < ApplicationController
   skip_before_action :authenticate_request
   def index
-    render json: { "key": "value" }, status: :ok
+    render json: { key: 'value' }, status: :ok
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_request, only: [:create, :index]
+  skip_before_action :authenticate_request, only: %i[create index]
   before_action :set_user, only: %i[show destroy]
 
   # GET /users
